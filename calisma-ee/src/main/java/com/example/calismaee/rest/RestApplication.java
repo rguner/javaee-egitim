ea03f4e010fc0750a64c80bc76e3a08b569d559e
Ramazan Güner <ramazan.guner@turkcell.com.tr>
2021-10-04 15:52:13 +0300

    gitignore

---
+++ calisma-ee/src/main/java/com/example/calismaee/rest/RestApplication.java
@@ -0,0 +1,11 @@
+package com.example.calismaee.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/*
+Rest application olması için bir tane böyle class olması gerekiyor.
+ */
+@ApplicationPath("/rest")
+public class RestApplication extends Application {
+}
